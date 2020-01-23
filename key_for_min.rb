0fcb4d6717f66b93bc3103b6542d870edbace91e
Justin Pierce <jwpierce1984@gmail.com>
2020-01-23 15:35:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,11 @@ def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
   hash.each do |k, v|
-    if 
-    
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 
 end
